fe6173c4453a81f58e68f192fcd14dad71b6147a
Reda Chanaa <chanaa-reda@hotmail.fr>
2021-11-02 15:22:21 +0100

    update AFN

--- BackEnd/StandApp/urls.py
+++ BackEnd/StandApp/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    url(r'stat-csc/', csrf_exempt(views.StatFile), name='file'),
-    url(r'stat-csc-plus/', csrf_exempt(views.StatFilePlus), name='file'),
+    url(r'stat-csc/', csrf_exempt(views.StatCSC), name='file'),
+    url(r'stat-csc-plus/', csrf_exempt(views.StatCSCPLUS), name='file'),
     url(r'stat-alg/', csrf_exempt(views.StatALG), name='file'),
+    url(r'stat-ita-ctn/', csrf_exempt(views.StatITA), name='file'),
+    url(r'stat-tun-ctn/', csrf_exempt(views.StatTUNCTN), name='file'),
+    url(r'stat-tun-cl/', csrf_exempt(views.StatTUNCL), name='file'),
+    
 ]
