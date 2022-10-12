dc7196b276c602948ac4fe616965443c590f6fce
Alain <77416989+darkvad@users.noreply.github.com>
2022-10-12 11:01:26 +0200

    Update settings.py

--- BackEnd/DataValidation/settings/settings.py
+++ BackEnd/DataValidation/settings/settings.py
@@ -85,21 +85,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'DataValidation.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-'default': {
-# 'ENGINE': 'django.db.backends.sqlite3',
-# 'NAME': 'db.sqlite3',
-'ENGINE': 'django.db.backends.postgresql_psycopg2',
-'NAME': 'yield',
-'USER': 'postgres',
-'PASSWORD' : '2022',
-'HOST': 'localhost',
-'PORT': '5432',
-}
-}
 
 CACHES = {
     'default': {
