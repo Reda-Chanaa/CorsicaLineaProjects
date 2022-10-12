528d2995adf78986e70642bc0d41b43738bd45f4
Alain <77416989+darkvad@users.noreply.github.com>
2022-10-12 11:01:52 +0200

    Update dev.py

--- BackEnd/DataValidation/settings/dev.py
+++ BackEnd/DataValidation/settings/dev.py
@@ -1,1 +1,16 @@
 from DataValidation.settings.settings import *
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+'default': {
+# 'ENGINE': 'django.db.backends.sqlite3',
+# 'NAME': 'db.sqlite3',
+'ENGINE': 'django.db.backends.postgresql_psycopg2',
+'NAME': 'yield',
+'USER': 'postgres',
+'PASSWORD' : '2022',
+'HOST': 'localhost',
+'PORT': '5432',
+}
+}
