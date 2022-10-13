1f3e06c6512d7807d83bbd430bce38f81118d78c
Alain <77416989+darkvad@users.noreply.github.com>
2022-10-13 14:17:36 +0200

    Update __init__.py

--- BackEnd/DataValidation/settings/__init__.py
+++ BackEnd/DataValidation/settings/__init__.py
@@ -2,12 +2,15 @@ import os
 import json
 
 
-CONFIG_FILE = './config.json'
+CONFIG_FILE = '/etc/config.json'
+print(CONFIG_FILE)
 
 try:
     with open(CONFIG_FILE) as config_file:
         config = json.load(config_file)
-        PROD=config['PROD']
+        PROD = config['PROD']
+        print(config['PROD'])
+        print(PROD)
     from .prod import *
     print("prod")
 
@@ -15,5 +18,7 @@ try:
 except KeyError:
     print("dev")
     from .dev import *
+    print("apres dev")
 
 '''SECRET_KEY = config['SECRET_KEY']'''
+
